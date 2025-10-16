e877525b2ef98105a6f2349aff9d404d848ca747
Anton Demin <programsgb2@mail.ru>
2025-10-16 19:59:17 +0700

    test Error

--- Stemi.WebAPI/Features/Schedule/Queries/GetSchedule/GetScheduleRequest.cs
+++ Stemi.WebAPI/Features/Schedule/Queries/GetSchedule/GetScheduleRequest.cs
@@ -6,9 +6,7 @@ namespace Stemi.WebAPI.Features.Schedule.Queries.GetSchedule
 	{
 		[Range(1, 10)]
 		public int? Corpus { get; set; }
-
 		public DateTime? Date { get; set; }
-
 		public bool Next { get; set; } = false;
 	}
 }
---
+++ Stemi.WebAPI/Migrations/20251016121518_updateTableLessonsCorpus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stemi.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateTableLessonsCorpus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Corpus",
+                table: "Lessons",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Corpus",
+                table: "Lessons");
+        }
+    }
+}
